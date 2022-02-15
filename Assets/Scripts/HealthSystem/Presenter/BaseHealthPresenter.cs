5e7bbb64a92ec29af1d7d447f73f3095d0240ed9
Artur-Sasnin <Artur.Sasnin@itechart-group.com>
2022-02-15 18:06:56 +0300

    create health system

---
+++ Assets/Scripts/HealthSystem/Presenter/BaseHealthPresenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public abstract class BaseHealthPresenter : BasePresenter<IHealthView>
+{
+    public virtual event Action OnDeathEvent;
+    public BaseHealthPresenter(IHealthView view) : base(view)
+    {
+    }
+    
+    public abstract void InitModel(int currentHealth, int maxHealth);
+    public abstract void Heal(int value);
+    public abstract void TakeDamage(int value);
+}
